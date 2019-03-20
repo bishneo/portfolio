dbcc292dd2439a44202eee83678d9ed4e640196b
bishneo <neogy.b@husky.neu.edu>
2019-03-20 01:21:10 -0400

    Updates

---
+++ precache-manifest.c95b6bb7a5d7ca4cf75ba824558a0ddb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a0cb1c3083e1691e995286f882732bc",
+    "url": "/portfolio/static/media/rockclimb.5a0cb1c3.jpg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "45ac377690461d2be60b",
+    "url": "/portfolio/static/js/main.ba2c2f09.chunk.js"
+  },
+  {
+    "revision": "14acf09c292933df8232",
+    "url": "/portfolio/static/js/2.ca20e315.chunk.js"
+  },
+  {
+    "revision": "45ac377690461d2be60b",
+    "url": "/portfolio/static/css/main.7a450c61.chunk.css"
+  },
+  {
+    "revision": "134d3ca84f5af2d167560ab0ea4f5aed",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff7c2a1aa8440ad2a1f8e0ec31ab5689.js"
+  "/portfolio/precache-manifest.c95b6bb7a5d7ca4cf75ba824558a0ddb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
