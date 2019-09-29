e88592338e59dd0e44dec14db8f556d855d0e0a2
bishneo <neogy.b@husky.neu.edu>
2019-09-29 15:11:40 -0400

    Updates

--- precache-manifest.3d665b18f6ed9d7c03c2aa95ebda28ec.js
+++ precache-manifest.3d665b18f6ed9d7c03c2aa95ebda28ec.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
   },
   {
-    "revision": "e792683f0e731743d7a4",
-    "url": "/portfolio/static/js/main.99866427.chunk.js"
+    "revision": "dba5f1bd5f9a2ef7949a",
+    "url": "/portfolio/static/js/main.4c309ef1.chunk.js"
   },
   {
     "revision": "1c4db8c5f9576c34ae8f",
     "url": "/portfolio/static/js/2.5dcd59bd.chunk.js"
   },
   {
-    "revision": "e792683f0e731743d7a4",
-    "url": "/portfolio/static/css/main.e4efe54f.chunk.css"
+    "revision": "dba5f1bd5f9a2ef7949a",
+    "url": "/portfolio/static/css/main.8731acd3.chunk.css"
   },
   {
-    "revision": "fa7b0c898d83eef9aff7cea0d1872178",
+    "revision": "676d26053e0436636b85e73792dc51a5",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7e1e7828ce548f1e2d7ea42da930aa21.js"
+  "/portfolio/precache-manifest.3d665b18f6ed9d7c03c2aa95ebda28ec.js"
 );
 
 workbox.clientsClaim();
