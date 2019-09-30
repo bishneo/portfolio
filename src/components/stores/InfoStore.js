41db239ca1e5ae9a9f38680755390d19bb47193b
bishneo <neogy.b@husky.neu.edu>
2019-09-29 20:38:06 -0400

    modified resume

--- src/components/stores/InfoStore.js
+++ src/components/stores/InfoStore.js
@@ -5,6 +5,6 @@ const InfoStore = {
     linkedin: "https://www.linkedin.com/in/bishwarup-neogy-57347a62/",
     location: "https://goo.gl/maps/UKfE238B4iqDaZXG9",
     address: "10 Allston St, Boston, MA - 02134",
-    resume: "res/files/Bishwarup Neogy_Resume(softdev-ai).pdf"
+    resume: "res/files/Bishwarup_Neogy_Resume(softdev-ai).pdf"
 }
 export default InfoStore;
