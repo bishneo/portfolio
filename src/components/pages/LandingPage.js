46191dfab2e00ffc098964bfa686a88dc41291ea
bishneo <neogy.b@husky.neu.edu>
2019-09-29 20:25:42 -0400

    changing bg image

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -3,8 +3,8 @@ class LandingPage extends Component {
     render() {
         return (
             <div className="landing-page sub-panel valign-wrapper">
-                <div className="container intro-box center grey-text text-lighten-4">
-                    <h1 className="red-text">Hi,</h1>
+                <div className="container intro-box center grey-text text-lighten-4" style={{marginTop:'20%'}}>
+                    <h1 className="pink-text text-darken-2">Hi,</h1>
                     <h3>I'm Bishwarup</h3>
                     <hr style={{ border: '1px solid white', width: '70%' }} />
                     <h4>Graduate Student | Web Developer | AI Enthusiast</h4>
