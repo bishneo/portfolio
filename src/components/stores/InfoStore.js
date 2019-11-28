8f3ae85084c33ef5a99b1626148b7cb408022cbb
bishneo <bish.neogy@gmail.com>
2019-11-28 17:55:53 -0500

    updating resume link

--- src/components/stores/InfoStore.js
+++ src/components/stores/InfoStore.js
@@ -5,6 +5,6 @@ const InfoStore = {
     linkedin: "https://www.linkedin.com/in/bishwarup-neogy-57347a62/",
     location: "https://goo.gl/maps/UKfE238B4iqDaZXG9",
     address: "10 Allston St, Boston, MA - 02134",
-    resume: "https://drive.google.com/file/d/1DaFswKwwJN5Ngvr5DE3lmfSA8ondRYdb/view"
+    resume: "https://drive.google.com/open?id=1mE1NRRz9yr5vD58S6tjTkhZgHAcXQAzP/view"
 }
 export default InfoStore;
